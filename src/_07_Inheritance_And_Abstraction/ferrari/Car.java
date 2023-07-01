6808710d6df870c91bd7f5dbbaefdfd2bb823b38
kristina <kristina.danovska@gmail.com>
2023-07-01 22:53:32 +0300

    inheritance and abstraction

---
+++ src/_07_Inheritance_And_Abstraction/ferrari/Car.java
@@ -0,0 +1,6 @@
+package _07_Inheritance_And_Abstraction.ferrari;
+
+public interface Car {
+    String brakes();
+    String gas ();
+}
---
+++ src/_07_Inheritance_And_Abstraction/ferrari/Main.java
@@ -0,0 +1,15 @@
+package _07_Inheritance_And_Abstraction.ferrari;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        Car ferrari = new Ferrari(name);
+        System.out.println(ferrari.toString());
+
+    }
+}
