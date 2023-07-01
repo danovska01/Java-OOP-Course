b7ce5dd3cee687af2753297a8a47ab7de504b5b4
kristina <kristina.danovska@gmail.com>
2023-07-01 23:19:34 +0300

    inheritance and abstraction

---
+++ src/defineAnInterfacePerson/Person.java
@@ -0,0 +1,7 @@
+package defineAnInterfacePerson;
+
+public interface Person {
+
+    String getName();
+    int getAge();
+}
---
+++ src/multipleImplementation/Person.java
@@ -0,0 +1,7 @@
+package multipleImplementation;
+
+public interface Person {
+
+    String getName();
+    int getAge();
+}
